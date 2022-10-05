5fdf51c53d56924275f90eb22d9206554c09ce36
chalith <desaman.chalith@gmail.com>
2022-10-05 18:18:09 +0530

    Initial implementations of contract deploy

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# ev-devkit
-Developer toolkit for Evernode smart contract deployment.
+# Evernode developer kit
+Evernode uses HotPocket as its smart contract engine. HotPocket smart contracts can be developed using any POSIX-compliant language/framework. To make it easy to develop and deploy HotPocket smart contracts on Evernode network, you can use Evernode developer kit.
+
+## Installation
+
+### Prerequisites
+HotPocket developer kit requires you to install [NodeJs](https://nodejs.org/en/) on your development machine.
+
+### Supports cross platform
+This is a npm global package which supports both Linux and Windows
+1. Install [prerequisites](#prerequisites).
+2. Run the following command to install hpdevkit on your machine.
+    ```
+    npm i -g evdevkit
+    ```
+
+## Updates
+Update `evdevkit` to the latest and update the supporting docker images.
+
+Run one of following commands to update evdevkit.
+    ```
+    npm update -g evdevkit
+    ```
+
+## Uninstall
+Uninstall `evdevkit` and the supporting docker images and containers.
+
+- Using hpdevkit CLI
+    ```
+    npm uninstall -g evdevkit
+    ```
+
+_**NOTE:** In Linux platforms, for Installation, Update and Uninstallation you'll need root privileges. Add `sudo` to above commands._
---
+++ index.js
@@ -0,0 +1,66 @@
+#! /usr/bin/env node
+
+const { program, Argument } = require('commander');
+const { version, list, acquire, host, bundle, keygen, deploy } = require('./lib/command-handler');
+
+program
+    .command('version')
+    .description('See evdevkit version')
+    .action(version);
+
+program
+    .command('list')
+    .description('List active hosts in Evernode.')
+    .option('-l, --limit [limit]', 'List limit')
+    .action(list);
+
+program
+    .command('host')
+    .description('View host info')
+    .argument('<host-address>', 'Host XRPL account address')
+    .action(host);
+
+program
+    .command('keygen')
+    .description('Generate user key pair for HotPocket')
+    .action(keygen);
+
+program
+    .command('acquire')
+    .description('Acquire instance in Evernode')
+    .argument('<tenant-address>', 'Tenant XRPL account address')
+    .argument('<tenant-secret>', 'Tenant XRPL account secret')
+    .option('-h, --host [host]', 'Host to acquire')
+    .option('-o, --owner [owner]', 'Public key of the owner')
+    .option('-m, --moments [moments]', 'Life moments')
+    .option('-c, --contract-id [contract-id]', 'Contract id')
+    .option('-i, --image [image]', 'Instance image')
+    .action(acquire);
+
+program
+    .command('bundle')
+    .description('Create contract bundle from contract')
+    .argument('<contract-path>', 'Absolute path to the contract directory to be bundled')
+    .argument('<instance-public-key>', 'Public key of the Evernode instance')
+    .argument('<contract-bin>', 'Contract binary name')
+    .argument('<contract-bin-args>', 'Contract binary arguments')
+    .action(bundle);
+
+program
+    .command('deploy')
+    .description('Deploy contract to a Evernode instance')
+    .argument('<contract-bundle-path>', 'Absolute path to the contract bundle')
+    .argument('<instance-ip>', 'IP address of the Evernode instance')
+    .argument('<contract-user-port>', 'User port of the instance')
+    .argument('<user-key-file>', 'Absolute path to the user key file')
+    .action(deploy);
+
+try {
+    program.parse();
+}
+catch (e) {
+    // Console outputs will be handled inside command functions.
+    // Log the exception if not a console output.
+    if (!('stdout' in e) && !('stderr' in e))
+        console.error(e);
+}
