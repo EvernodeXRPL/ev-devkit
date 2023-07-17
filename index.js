082f8d4094d9fc9c148b3de93c7478f68fca5efc
chalith <desaman.chalith@gmail.com>
2023-07-17 10:35:29 +0530

    Fixed multisig bundle upload issue

--- index.js
+++ index.js
@@ -107,9 +107,10 @@ program
     .option('-m, --moments [moments]', 'Life moments')
     .option('-c, --contract-id [contract-id]', 'Contract id')
     .option('-i, --image [image]', 'Instance image')
-    .option('-f, --file-path [file-path]', 'File path of preferred host account list (in line-by-line format)')
+    .option('-h, --hosts-file-path [hosts-file-path]', 'File path of preferred host account list (in line-by-line format)')
     .option('--multisig', 'Requirement of making a cluster with multiple signer nodes')
     .option('-s, --signers [signers]', 'JSON file path of signer details')
+    .option('-c, --signer-count [signer-count]', 'Signer count for multi signing')
     .option('-q, --quorum [quorum]', 'Quorum of the cluster with multiple signer nodes (within the valid range (0,1])')
     .action(clusterCreate);
 
