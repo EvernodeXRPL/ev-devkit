672cdb7af9a31d58f826d30572304d30d665a65f
chalith <desaman.chalith@gmail.com>
2023-07-21 17:44:37 +0530

    Updated latest hotpocket image version

--- appenv.js
+++ appenv.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 // Throw errors if the env value is required.
 const appenv = {
-    instanceImage: 'evernodedev/sashimono:hp.0.6.2-ubt.20.04-njs.16',
+    instanceImage: 'evernodedev/sashimono:hp.0.6.2-ubt.20.04-njs.20',
     get tenantSecret() {
         if (!process.env.EV_TENANT_SECRET)
             throw 'EV_TENANT_SECRET environment variable has not been set.';
