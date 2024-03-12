0cb515005d2f79e1352faec466f237e5124e31ef
chalith <desaman.chalith@gmail.com>
2024-03-12 17:33:59 +0530

    Added failure handling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 const { program } = require('commander');
-const { version, list, acquire, host, bundle, keygen, deploy, acquireAndDeploy, clusterCreate, extendInstance, audit } = require('./lib/command-handler');
+const { version, list, acquire, host, bundle, keygen, deploy, acquireAndDeploy, clusterCreate, extend, extendInstance, audit } = require('./lib/command-handler');
 
 const ENV_TEXT = 'Environment Variables:';
 const REQUIRED_TEXT = 'Required:';
@@ -20,6 +20,9 @@ program
     .command('list')
     .description('List active hosts in Evernode.')
     .option('-l, --limit [limit]', 'List limit')
+    .option('-o, --order-by [order-by]', 'Order by key')
+    .option('-d, --desc [desc]', 'Order by descending manor')
+    .option('-p, --props [props]', 'Comma separated properties to show')
     .action(list);
 
 program
@@ -118,6 +121,13 @@ program
     .option('--signer-quorum [signer-quorum]', 'Quorum of the cluster with multiple signer nodes (within the valid range (0,1])')
     .option('-e, --evr-limit [evr-limit]', 'Maximum amount of EVRs to be spent on instance acquisitions')
     .action(clusterCreate);
+
+program
+    .command('extend')
+    .description('Extend instances')
+    .argument('<instances-file-path>', 'File path of instance list (in line-by-line format <host-address>:<instance-name>)')
+    .option('-m, --moments [moments]', 'Instance Life In Moments')
+    .action(extend);
 
 program
     .command('extend-instance')
